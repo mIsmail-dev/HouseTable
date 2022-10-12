8aed00f43bd3f57893098e4eb0e47881b700db2f
mIsmail-dev <muhammad.ismail@ocloudsolutions.net>
2022-10-12 15:56:26 +0500

    Removed Comments.

--- utils/utils.js
+++ utils/utils.js
@@ -1,47 +1,41 @@
-// will return today Date
 const getTodayDate = () => {
     let today = new Date()
-    const dd = String(today.getDate() + 1).padStart(2, '0') // added 1 bcz it was giving one previous day like if today is 7 it will give 6.
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const dd = String(today.getDate() + 1).padStart(2, '0')
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
     const yyyy = today.getFullYear()
     today = mm + '-' + dd + '-' + yyyy;
     return new Date(today)
 }
 
-// will return second Date w.r.t to numOfWeeks. Basically Current Date - numofWeeks.
 const getSecondDate = (numOfWeeks, todayDate) => {
     let secondDate = new Date()
     secondDate.setDate(todayDate.getDate() - numOfWeeks * 7)
     return secondDate
 }
 
-// will convert iso string into a string and will return it
 const getStringDate = (today) => {
-    const dd = String(today.getDate()).padStart(2, '0') // added 1 bcz it was giving one previous day like if today is 7 it will give 6.
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const dd = String(today.getDate()).padStart(2, '0')
+    const mm = String(today.getMonth() + 1).padStart(2, '0')
     const yyyy = today.getFullYear()
     today = mm + '-' + dd + '-' + yyyy;
     return today
 }
 
-// will return numOfWeeks w.r.t time period(weekly,  monthly, yearly)
 const getNumOfWeeks = (period) => {
     if(period === "weekly") {
         return 1
     } else if(period === "monthly") {
         return 4
     } else {
-        return 12 // for yearly
+        return 12
     }
 }
 
-// will check if the given currency is valid or not
 const validCurrency = (currency) => {
     const validCurrency = ['usd', 'eur']
     return validCurrency.includes(currency)
 }
 
-// Will return Paid & UnPaid Fee of all appointments
 const getFee = (appointments, requiredCurrency) => {
     const currencyConversion = {
         usd: 1.02, // Usd to eur
@@ -49,8 +43,6 @@ const getFee = (appointments, requiredCurrency) => {
     }
 
     const fee = {}
-
-    // Finding the sum of Unpaid Bills
     fee.unpaid = appointments.reduce((acc, appointment) => {
         if(!appointment.isPaid) {
             if(appointment.currency !== requiredCurrency) {
@@ -61,8 +53,6 @@ const getFee = (appointments, requiredCurrency) => {
         }
         return acc
     }, 0)
-
-    // Finding the sum of paid Bills
     fee.paid = appointments.reduce((acc, appointment) => {
         if(appointment.isPaid) {
             if(appointment.currency !== requiredCurrency) {
@@ -77,7 +67,6 @@ const getFee = (appointments, requiredCurrency) => {
     return fee
 }
 
-// will return most Popular pet
 const getPopularPet = (patients) => {
     let max = 0;
     const popularPet = patients.reduce((acc, patient) => {
@@ -93,7 +82,6 @@ const getPopularPet = (patients) => {
   return popularPet
 }
 
-// will return every pet Details
 const getEveryPetDetails = (patients) => {
     const petsDetail = patients.reduce((acc, patient) => {
         const petDetail = {};
